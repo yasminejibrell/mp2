6263e807ad81937f3e343f2d8dce5c9d549ef116
Yasmine Jibrell <yjibrell@bu.edu>
2024-10-06 13:24:55 -0400

    edits

---
+++ src/interfaces/KanyeQuotes.ts
@@ -0,0 +1,16 @@
+
+
+// src/interfaces/Quotes.ts
+export interface KanyeQuotes{
+    quote: string;
+}
+
+
+
+
+
+
+    // id: number;
+    // image: string;
+    // species: string;
+    // status: string;
